f1ea0982d5e1115ec52d6856c8f374f7e864856f
melber <melber369@gmail.com>
2018-10-02 21:01:10 +1000

    passing the current player from function to function in js

--- js/app.js
+++ js/app.js
@@ -10,36 +10,43 @@ var winningCombos = [
   [3,5,7]
 ];
 var player1Moves = [];
-var winningMoveCounter = 0;
+var player2Moves = [];
 
-function handleClick(event) {
-  event.target.classList.toggle('player1');
+var currentPlayer = 'player2';
+
+function handleClick(event, currentPlayer) {
+  event.target.classList.toggle(currentPlayer);
   event.target.classList.toggle('clicked');
-  player1Moves.push(Number(event.target.id));
-  if(player1Moves.length >= 3) {
-    checkWinningCombo();
+  window[currentPlayer + 'Moves'].push(Number(event.target.id));
+  
+  if(window[currentPlayer + 'Moves'].length >= 3) {
+    checkWinningCombo(currentPlayer);
   }
 }
 
-function checkWinningCombo() {
+function checkWinningCombo(currentPlayer) {
+  var winningMoveCounter = 0;
+  var currentPlayerMoves = window[currentPlayer + 'Moves'];
   for(var i = 0; i < winningCombos.length; i++) {
-    player1Moves.forEach(function(move) {
-      if(winningCombos[i].includes(move)) {
+    for(var j = 0; j < currentPlayerMoves.length; j++) {
+      if(winningCombos[i].includes(currentPlayerMoves[j])) {
         ++winningMoveCounter;
       }
-    })
+    }
     if(winningMoveCounter === 3) {
-      endGame();
+      endGame(currentPlayer);
       break;
     }
     winningMoveCounter = 0;
   }
 }
 
-function endGame() {
-  console.log("you've won!")
+function endGame(currentPlayer) {
+  console.log(currentPlayer + ' has won!');
 }
 
 boxes.forEach(function(box){
-  box.addEventListener('click', handleClick);
+  box.addEventListener('click', function() {
+    handleClick(event, currentPlayer);
+  });
 })
